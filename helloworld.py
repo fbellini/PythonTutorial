6a571da506fa96d814b55f21759b40fc09aa5348
Francesca Bellini <fbellini@cern.ch>
2020-03-31 19:06:49 +0200

    added function for translator to pyg latin

--- helloworld.py
+++ helloworld.py
@@ -11,4 +11,25 @@ print ('Last update: ', now.strftime('%d %B %Y %H:%M:%S'))
 #24-hour format
 #print(now.strftime('%Y/%m/%d %H:%M:%S'))
 #12-hour format
-#print(now.strftime('%Y/%m/%d %I:%M:%S')) 
+#print(now.strftime# ('%Y/%m/%d %I:%M:%S')) 
+
+
+#define function English to Pyg Latin translator
+def english2pyg(original) :
+    pyg = 'ay'
+    new_word = 'empty'
+    #original = raw_input('Enter a word:')
+    if len(original) > 0 and original.isalpha():
+        #print original
+        word = original.lower()
+        first = original[0]
+        new_word = original[1:len(original)] + first + pyg
+        print (new_word)
+    else:
+        print ('empty')
+    return new_word
+
+
+#english2pyg("ciccio")
+
+
