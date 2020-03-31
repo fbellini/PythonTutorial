d84b10f52fc68e7c519bd4118212b73b9f2135aa
Francesca Bellini <fbellini@cern.ch>
2020-03-31 17:17:59 +0200

    added creation time

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,12 @@
 from datetime import datetime
+import os.path, time
+
 now = datetime.now()
 current_year = now.year
 current_month = now.month
 current_day = now.day
 
+print("Created: %s" % time.ctime(os.path.getmtime("/Users/fbellini/projects/pythontutorial/helloworld.py")))
 print ('Last update: ', now.strftime('%d %B %Y %H:%M:%S'))
 #24-hour format
 #print(now.strftime('%Y/%m/%d %H:%M:%S'))
